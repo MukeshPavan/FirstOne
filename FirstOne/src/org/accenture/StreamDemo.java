0173d9f5a0456364bc9c4e3a113d2f43717cb461
kothapalli.m.pavan <mukeshpavan001@gmail.com>
2022-02-18 13:08:46 +0530

    Committing the project

--- FirstOne/src/org/accenture/StreamDemo.java
+++ FirstOne/src/org/accenture/StreamDemo.java
@@ -1,9 +1,6 @@
 package org.accenture;
-
 import java.util.*;
 import java.util.stream.Collectors;
-
-
 @SuppressWarnings("ALL")
 public class StreamDemo {
     public static void main(String[] args) {
@@ -15,12 +12,11 @@ public class StreamDemo {
         i.add(10);
         i.add(22);
         i.add(246);
-        System.out.println(i); //Printing set of integers
+        System.out.println(i); // Printing set of integers
         List<Integer> i2 = i.stream().filter(x->x%2!=0).collect(Collectors.toList());
-        System.out.println(i2); //Set of odd numbers from given set of integers
-        System.out.println("----------------------");
+        System.out.println(i2); // Printing Set of odd numbers from given set of integers
+        /* In a class(in school) some example scenarios*/
         ArrayList<Integer> marks = new ArrayList<>();
-
         marks.add(53);
         marks.add(30);
         marks.add(45);
@@ -28,26 +24,22 @@ public class StreamDemo {
         marks.add(33);
         marks.add(74);
         marks.add(63);
-        System.out.println(marks);//Printing students marks
-        long noOFailedStudents = marks.stream().filter(x->x<36).count();
-        System.out.println("no of failed students are: "+noOFailedStudents);
-        List<Integer> updatedMarks = marks.stream().map(x->x+6).collect(Collectors.toList());
-        System.out.println(updatedMarks);
-        System.out.println("----------------------");
-        long noOFFailedStudents = updatedMarks.stream().filter(x->x<36).count();
-        System.out.println("no of failed students after adding grace marks are: "+noOFFailedStudents);
-        List<Integer> sortedMarks = marks.stream().sorted().collect(Collectors.toList());
-        System.out.println(sortedMarks);
-        List<Integer> descSortedMarks =sortedMarks.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-        System.out.println(descSortedMarks);
-        System.out.println("----------------------");
-        Integer minValue = descSortedMarks.stream().min(Comparator.naturalOrder()).get();
-        System.out.println(minValue);
-        Integer maxValue=descSortedMarks.stream().max(Comparator.naturalOrder()).get();
-        System.out.println(maxValue);
-        System.out.println("------------------");
-        //________________________________________
-        //****************************************
+        System.out.println(marks);// Printing students marks
+        long noOFailedStudents = marks.stream().filter(x->x<36).count();// used to filter the marks of students
+        System.out.println("no of failed students are: "+noOFailedStudents);// Printing the no of failed students
+        List<Integer> updatedMarks = marks.stream().map(x->x+6).collect(Collectors.toList()); // Teacher used to update the marks of students by adding grace marks.
+        System.out.println(updatedMarks); // Printing Updated students marks
+        long noOFFailedStudents = updatedMarks.stream().filter(x->x<36).count(); //again counting if any failed students smissed
+        System.out.println("no of failed students after adding grace marks are: "+noOFFailedStudents); //No one is there all are passed
+        List<Integer> sortedMarks = marks.stream().sorted().collect(Collectors.toList()); //sorting the marks (ascending order)
+        System.out.println(sortedMarks); //used to print sorted marks
+        List<Integer> descSortedMarks =sortedMarks.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());//sorting the marks (descending order)
+        System.out.println(descSortedMarks); //used to print sorted marks
+        Integer minValue = descSortedMarks.stream().min(Comparator.naturalOrder()).get(); // finding minimum marks of student
+        System.out.println(minValue);//printing the minimum value
+        Integer maxValue=descSortedMarks.stream().max(Comparator.naturalOrder()).get();// finding maximum marks of student
+        System.out.println(maxValue);//printing the maximum value
+        /*  using strings some example scenarios*/
         ArrayList<String> y = new ArrayList<>();
         y.add("dog");
         y.add("frog");
@@ -56,14 +48,10 @@ public class StreamDemo {
         y.add("lion");
         y.add("cheetah");
         System.out.println(y); //Printing set of words
-        List<String> sortedAnimals = y.stream().sorted().collect(Collectors.toList());
-        System.out.println(sortedAnimals);
-        List<String> descSortedAnimals = y.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-        System.out.println(descSortedAnimals);
-        System.out.println("----------------------");
-        //-------------------------------------------------------
-        //________________________________________________________________________
-        //*************************************************************************
+        List<String> sortedAnimals = y.stream().sorted().collect(Collectors.toList());//sorting the animals(ascending order)
+        System.out.println(sortedAnimals);//Printing sorted values
+        List<String> descSortedAnimals = y.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());//sorting the animals(descending order)
+        System.out.println(descSortedAnimals);//Printing sorted values
         Comparator<String> c = (s1,s2)->{
             if (s1.length()<s2.length())
                 return -1;
@@ -71,8 +59,8 @@ public class StreamDemo {
                 return 1;
             else
                 return s1.compareTo(s2);
-        };
-        List<String> sortedAnimalsBasedOnLength = y.stream().sorted(c).collect(Collectors.toList());
-        System.out.println(sortedAnimalsBasedOnLength);
+        };//method to sort based on length
+        List<String> sortedAnimalsBasedOnLength = y.stream().sorted(c).collect(Collectors.toList());//collecting the sorted animals based on length
+        System.out.println(sortedAnimalsBasedOnLength);//printing the sorted animals
     }
 }
